7bef464c69be7ac993a82b5d6a88d555423cdd32
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-10-07 18:43:38 +0400

    Add IBOutlets & IBActions in QuestionsViewController

--- PersonalQuizSelf/ViewControllers/QuestionsViewController.swift
+++ PersonalQuizSelf/ViewControllers/QuestionsViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    @IBOutlet weak var questionProgressView: UIProgressView!
+   
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+    
 }
