d9129ff1ef28d9445ef77e6315165c735c9b493a
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-10-08 12:55:21 +0400

    new interface element, IBOutlets and IBActions

---
+++ PersonalQuizSelf/ViewControllers/AnswersViewController.swift
@@ -0,0 +1,73 @@
+//
+//  AnswersViewController.swift
+//  PersonalQuizSelf
+//
+//  Created by Николай Христолюбов on 08.10.2022.
+//
+
+import UIKit
+
+class AnswersViewController: UIViewController {
+
+    @IBOutlet weak var questionProgressView: UIProgressView!
+   
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
